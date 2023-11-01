9913094cc9c8e7cfb8dd413f75f11b6661e4b9b2
Rafael Macedo <rafaelrodriguesmacedo2004@gmail.com>
2023-10-31 21:44:30 -0300

    feature: markdown editor page

---
+++ frontend/src/app/send/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useState } from 'react';
+import React from 'react';
+import { api } from '@/lib/api';
+import MDEditor from '@uiw/react-md-editor';
+
+export default function SendPage() {
+    const [value, setValue] = React.useState("**Hello world!!!**");
+
+    return (
+        <div className='text-center'>
+            <div className='mt-[15vh]'>
+                <MDEditor
+                    value={value}
+                    onChange={setValue}
+
+                />
+            </div>
+        </div>
+    );
+}
